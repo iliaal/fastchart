89f5dd24c3a38f8a291dd5035fa24518fdb4fdef
Ilia Alshanetsky <ilia@ilia.ws>
2026-07-07 18:31:45 -0400

    Release 1.5.0

--- php_fastchart.h
+++ php_fastchart.h
@@ -21,7 +21,7 @@
 
 #include "fastchart_graph.h"
 
-#define PHP_FASTCHART_VERSION "1.4.0"
+#define PHP_FASTCHART_VERSION "1.5.0"
 
 extern zend_module_entry fastchart_module_entry;
 #define phpext_fastchart_ptr &fastchart_module_entry
